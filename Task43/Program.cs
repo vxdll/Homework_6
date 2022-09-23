3ad8de5cfe45bf7810c57d364e2bf1850f58d263
Григорий Васякин <vxdll.39@gmail.com>
2022-09-23 15:29:35 +0200

    Task43

---
+++ Task43/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+ y = k1 * x + b1
+ y = k2 * x + b2;
+значения b1, k1, b2 и k2 задаются пользователем.*/
+
+Console.WriteLine ("Введите значение k1: ");
+int yk1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите значение b1: ");
+int yb1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите значение k2: ");
+int yk2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите значение b2: ");
+int yb2 = Convert.ToInt32(Console.ReadLine());
+
+if (yk1 == yk2 & yb1 == yb2){
+    Console.WriteLine("При указанных k1, b1 и k2, b2 прямые будут совпадать!");
+} else if (yk1 == yk2 )
+{
+    Console.WriteLine("При указанных k1 и k2 прямые не будут иметь точек пересечения, прямые параллельны!");
+} else{
+    // Нахождения ОХ точки пересечения:
+    int ox = (yb2 - yb1) / (yk1 - yk2);
+    // Нахождения ОY точки пересечения:
+    int oy = (yk1 * yb2 - yk2 * yb1) / (yk1 - yk2);
+    Console.WriteLine($"Точка пересечения имеет координату -> ({ox}, {oy})");
+}
